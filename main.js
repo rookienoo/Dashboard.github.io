923e8e37a0db0dfd88b0e7796e89c74acd52c712
rookienoo <159983659+rookienoo@users.noreply.github.com>
2024-02-20 09:25:52 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+let myImage = document.querySelector("img");
+
+myImage.onclick = function () {
+  let mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/caicai1.jpg") {
+    myImage.setAttribute("src", "images/caicai2.jpg");
+  } else {
+    myImage.setAttribute("src", "images/caicai1.jpg");
+  }
+};
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName() {
+    let myName = prompt("请输入你的名字。");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = "Mozilla 酷毙了，" + myName;
+  }
+  if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem("name");
+    myHeading.textContent = "Mozilla 酷毙了，" + storedName;
+  }
+  myButton.onclick = function () {
+    setUserName();
+  };
+      
